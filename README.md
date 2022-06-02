031cdf15dc97ff658b8ff87fbb839b7f4941a308
Kyle Canamar <canamar.kyle@gmail.com>
2022-06-01 17:10:57 -0700

    creates README and cleans up server.js more

---
+++ README.md
@@ -0,0 +1,10 @@
+Follows MVC layout and works to modulerize server components
+
+### Api Routes
+
+|Routes|Method|EndPoints|Expected Result|
+|------|:---:|:---:|:----|
+|Index|GET|/food|Gets all menu items|
+|Create|POST|/food|Creates new menu item|
+|Update|PUT|/food/:id|Updates existing menu item|
+|Delete|DELETE|/food/:id|Removes menu item from database|
--- server.js
+++ server.js
@@ -2,66 +2,20 @@
 // Setup - Import deps and create app object
 ////////////////////////
 require("dotenv").config()
+require("./config/db")
 const express = require("express")
-const { PORT, DATABASE_URL } = process.env
+const Middleware = require('./middleware/mid')
+const PosRouter = require('./routes/food')
 const app = express()
-const morgan = require("morgan")
-const cors = require("cors")
-const mongoose = require("mongoose")
-const Food = require("./model/food")
+const { PORT, DATABASE_URL } = process.env
 //////////////////////
 // Declare Middleware
 //////////////////////
-app.use(cors())
-app.use(morgan("dev"))
-app.use(express.json())
-//////////////////////
-// Database Connection
-//////////////////////
-mongoose.connect(DATABASE_URL)
-mongoose.connection
-    .on("open", () => console.log("You are connected to MongoDB"))
-    .on("close", () => console.log("You are disconnected from MongoDB"))
-    .on("error", (error) => console.log(error))
+Middleware(app)
 ///////////////////////
 // Declare Routes and Routers 
 ///////////////////////
-// INDUCES - Index, New, Delete, Update, Create, Edit, Show
-app.get("/", (req, res) => {
-    res.send("Please add 'food' to the end of your Url.")
-})
-
-app.get("/food", async (req, res) => {
-    try {
-        res.json( await Food.find({}))
-    } catch (error) {
-        res.status(400).json(error)
-    }
-})
-
-app.post("/food", async (req, res) => {
-    try {
-        res.json( await Food.create(req.body))
-    } catch(error){
-        res.status(400).json(error)
-    }
-})
-
-app.delete("/food/:id", async (req, res) => {
-    try {
-        res.json( await Food.findByIdAndDelete(req.params.id))
-    } catch (error) {
-        res.status(400).json(error)
-    }
-})
-
-app.put("/food/:id", async (req, res) => {
-    try {
-        res.json( await Food.findByIdAndUpdate(req.params.id, req.body, {new: true}))
-    } catch (error) {
-        res.status(400).json(error)
-    }
-})
+app.use(PosRouter)
 ///////////////////////////
 // Server Listener
 ///////////////////////////
